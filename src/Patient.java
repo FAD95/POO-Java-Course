30175a8668673632c588dd3ea164335180dc6953
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-11-07 18:28:39 -0500

    Encapsulamiento

--- src/Patient.java
+++ src/Patient.java
@@ -1,12 +1,13 @@
 public class Patient {
-    String name;
-    String email;
-    String address;
-    String phoneNumber;
-    String birthday;
-    double weight;
-    double height;
-    String blood;
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthday;
+    private double weight;
+    private double height;
+    private String blood;
 
     Patient(String name, String email){
         this.name=name;
@@ -16,5 +17,73 @@ public class Patient {
     public void showPatient() {
         System.out.println("Name: "+name);
         System.out.println("Email: "+email);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getWeight() {
+        return weight+"kg.";
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getHeight() {
+        return height+"Mts.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
     }
 }
